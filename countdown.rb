144c8d2d398da02bc9d25dcd9c9b9e66c5c8d3fd
Dan-Vy Le <danvyle85@gmail.com>
2019-05-10 00:12:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@
 # count = 12
 
 def countdown(n)
-  while n >= 0
+  while n > 0
     puts "#{n} SECOND(S)!"
     count -= 1
   end
@@ -14,11 +14,16 @@ def countdown(n)
 end
 
 def countdown_with_sleep(n)
-  while n >= 0
+  while n > 0
     puts "#{n} SECOND(S)!"
     n -= 1
     sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
+def countdown(num)
 
+
+
+
+ 
